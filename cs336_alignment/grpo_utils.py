dde03b7d7d42dc14b2ed521bb028d6c8971972c3
CyaCoan <cyacoan@163.com>
2026-04-23 23:00:21 +0800

    Add compute_grpo_clip_loss

--- cs336_alignment/grpo_utils.py
+++ cs336_alignment/grpo_utils.py
@@ -65,3 +65,34 @@ def compute_naive_policy_gradient_loss(
     loss = -weighted_log_probs
 
     return loss
+
+def compute_grpo_clip_loss(
+    advantages: torch.Tensor,
+    policy_log_probs: torch.Tensor,
+    old_log_probs: torch.Tensor,
+    cliprange: float,
+) -> tuple[torch.Tensor, dict[str, torch.Tensor]]:
+    
+    log_ratio = policy_log_probs - old_log_probs
+    ratio = torch.exp(log_ratio)
+
+    surr1 = ratio * advantages
+
+    clipped_ratio = torch.clamp(ratio, 1.0 - cliprange, 1.0 + cliprange)
+    surr2 = clipped_ratio * advantages
+
+    loss = -torch.min(surr1, surr2)
+
+    with torch.no_grad():
+
+        clipped_mask = (surr2 < surr1).float()
+        clip_fraction = clipped_mask.mean()
+        
+        metadata = {
+            "clip_fraction": clip_fraction, # 非常关键：若此值接近 1.0，说明学习率过高或模型已停止学习
+            "ratio_mean": ratio.mean(),
+            "ratio_max": ratio.max(),
+            "ratio_min": ratio.min(),
+        }
+
+    return loss, metadata
